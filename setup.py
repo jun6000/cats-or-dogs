510a6c1067e91efdd68a70926402197707043151
Arjun Vishanth <arjunvishanth@gmail.com>
2023-09-23 22:27:31 +0530

    Moved all project files to src/ but still no luck.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version="0.0.3",  # Required
+    version="0.0.4",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
@@ -97,7 +97,7 @@ setup(
     keywords="cats, dogs, CNN, tensorflow, cats-or-dogs, model",  # Optional
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={"catsordogs": "src/catsordogs"},
+    # package_dir={"catsordogs": "src/catsordogs"},
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -107,7 +107,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(where="src/catsordogs"),  # Required
+    packages=find_packages(where="src"),  # Required
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
