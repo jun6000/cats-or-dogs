110aed0668ea468fb9bc52b196e202db3947fc10
Arjun Vishanth <arjunvishanth@gmail.com>
2023-09-23 22:17:33 +0530

    Added package_dir and specified correct path but still catsordogs module not found.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version="0.0.2",  # Required
+    version="0.0.3",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
@@ -97,7 +97,7 @@ setup(
     keywords="cats, dogs, CNN, tensorflow, cats-or-dogs, model",  # Optional
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    # package_dir={"": "src"},  # Optional
+    package_dir={"catsordogs": "src/catsordogs"},
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
